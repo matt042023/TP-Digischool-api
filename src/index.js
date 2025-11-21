9407530aa6a7dba12f24f1330486479404fdd2a7
Robin Cassan <robin.cassan@netc.eu>
2025-11-21 16:53:43 +0100

    fin des modifs et test avec seed Mathieu

--- src/index.js
+++ src/index.js
@@ -1,28 +1,62 @@
+// const express = require("express");
+// const connectDB = require("./config/database");
+// const classeRoutes = require("./routes/classeRoutes");
+// const matiereRoutes = require("./routes/matiereRoutes");
+
+// const app = express();
+// app.use(express.json());
+
+// // Connexion à MongoDB et démarrage du serveur seulement après succès
+// const PORT = 3000;
+
+// const startServer = async () => {
+//   try {
+//     await connectDB(); // attend que la connexion soit réussie
+//     app.listen(PORT, () => console.log(`Serveur démarré sur http://localhost:${PORT}`));
+//   } catch (err) {
+//     console.error("Impossible de démarrer le serveur :", err);
+//   }
+// };
+
+// startServer();
+
+// // Routes
+// app.use("/classes", classeRoutes);
+// app.use("/matieres", matiereRoutes);
+
+// // Route par défaut
+// app.get("/", (req, res) => res.send("API Digischool OK"));
+
+
+require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/database");
-const classeRoutes = require("./routes/classeRoutes");
-const matiereRoutes = require("./routes/matiereRoutes");
+const connectDB = require("./config/db");
+const classeRoutes = require("./routes/classeRoutes.js");
+const matiereRoutes = require("./routes/matiereRoutes.js");
+// const eleveRoutes = require("./routes/eleveRoutes.js");
+// const professeurRoutes = require("./routes/professeurRoutes.js");
+// const notesRoutes = require("./routes/notesRoutes.js");
+// const trimestreRoutes = require("./routes/trimestreRoutes.js");
 
 const app = express();
-app.use(express.json());
-
-// Connexion à MongoDB et démarrage du serveur seulement après succès
+connectDB();
 const PORT = 3000;
 
-const startServer = async () => {
-  try {
-    await connectDB(); // attend que la connexion soit réussie
-    app.listen(PORT, () => console.log(`Serveur démarré sur http://localhost:${PORT}`));
-  } catch (err) {
-    console.error("Impossible de démarrer le serveur :", err);
-  }
-};
-
-startServer();
+// Middleware pour parser le JSON
+app.use(express.json());
 
 // Routes
 app.use("/classes", classeRoutes);
 app.use("/matieres", matiereRoutes);
+// app.use("/eleves", eleveRoutes);
+// app.use("/professeurs", professeurRoutes);
+// app.use("/notes", notesRoutes);
+// app.use("/trimestres", trimestreRoutes);
 
 // Route par défaut
-app.get("/", (req, res) => res.send("API Digischool OK"));
+app.get("/", (req, res) => res.send("API Digischool  + MongoDB"));
+
+// Lancement du serveur
+app.listen(PORT, () =>
+  console.log(`Serveur démarré sur http://localhost:${PORT}`)
+);
